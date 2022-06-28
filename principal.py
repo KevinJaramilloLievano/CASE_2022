e347e4c257d42050fb879b97577a6d183e514cd8
KevinJaramilloLievano <kevinjaramillo@upp.edu.mx>
2022-06-28 10:30:37 -0500

    Ejemplo commit 2

--- principal.py
+++ principal.py
@@ -25,3 +25,5 @@ print("Soy Allan")
 print("Soy itzco")
 
 print("Yo y mi amigo (mi ego) debatiendo sobre si le gusto(Literalmente fue amabilidad) y sobre como enamorarla(llevamos mucho tiempo solos) David Morales")
+
+print("Ejemplo commit Kevo")
