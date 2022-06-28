9d7e8a814a40a12ec20e6432075f2e5c523fb97d
Ayanamicoco <angeldavidmc20001@outlook.com>
2022-06-28 09:40:14 -0500

    Yo y mi amigo (mi ego) debatiendo sobre si le gusto(Literalmente fue amabilidad) y sobre como enamorarla(llevamos mucho tiempo solos) David Morales

--- principal.py
+++ principal.py
@@ -1,15 +1,13 @@
 print("Kevin y python")
-<<<<<<< HEAD
+
 print("Allan y python")
-=======
+
 print("Omar Arteaga")
 
 print("Joseph Juarez")
 
 print("Hola soy Omar Falso")
-<<<<<<< HEAD
->>>>>>> dd3c186b36fd17759fb04990359e9922b4a0786d
-=======
+
 print("Que hay (Roberto)")
 
 print("Un lolsito o que banda?? (ulises)")
@@ -18,18 +16,12 @@ print("Un lolsito o que banda?? (ulises)")
 print("JIJIJIJAAÑA")
 
 print("awita de coco")
-<<<<<<< HEAD
->>>>>>> 494ad97e54661cc9484074cc8e3bcf960102f9d5
-<<<<<<< HEAD
-=======
->>>>>>> 991c5d6a0315093640d56eb1425f9986269efe40
->>>>>>> 487b491fa53f0324ab4e4cec94a066f68f7c44e4
-=======
 
 print("Hola soy Fer Fer Fer")
 print("Excelente servicio")
->>>>>>> c8f580037b85e6907bc9766b51397b434f3274a1
 
 print("Soy Allan")
 
 print("Soy itzco")
+
+print("Yo y mi amigo (mi ego) debatiendo sobre si le gusto(Literalmente fue amabilidad) y sobre como enamorarla(llevamos mucho tiempo solos) David Morales")
